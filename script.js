f82030670ea928af8723ac5451b06ab44ba2edc3
Mauricio Garcia <mauricio.garcia@mgar.com.br>
2023-11-21 14:22:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+/**
+ * Esse arquivo é um exemplo de como usar o editor Solvertype.
+ * Ele cria 6 editores Solvertype com dados diferentes.
+ * Devem ser usados pela aplicação para exibir e editar os dados.
+ * O componente Solvertype se ajusta ao tamanho (width e height) do elemento pai (div).
+ * O componente Solvertype está nos arquivos solvertype.js e solvertype.css.
+ */
+
+import Solvertype from './solvertype.js';
+
+// Exemplo de dados para serem exibidos 
+let data = [
+    `
+        Essa é a equação de Bhaskara: [latex]x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}[/latex]. Ela é usada para resolver as equações de segundo grau, também conhecidas como equações quadráticas.<p>
+        Aqui temos uma fórmula que envolve binomiais e frações: [latex]\\binom{n}{k} = \\frac{n!}{k!(n-k)!}[/latex]. Tem suas particularidades.<p>
+        Essa é uma equação complexa:<p> [latex]S(\\omega) \\frac{\\alpha g^2}{\\omega^5} e^{[ -0.74\\bigl\\{\\frac{\\omega U_\\omega 19.5}{g}\\bigr\\}^{\\!-4}\\,]}\\frac{\\alpha g^2}{\\omega^5} \\exp\\Bigl[ -0.74\\Bigl\\{\\frac{\\omega U_\\omega 19.5}{g}\\Bigr\\}^{\\!-4}\\,\\Bigr][/latex].<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+    `,
+
+    `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `,
+
+    `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+    `,
+
+    `
+        Iabore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `,
+
+    `
+        Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `,
+
+    `
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `,
+];
+
+// Cria objetos Solvertype com os dados acima
+// O objeto Solvertype se ajusta ao tamanho (width e height) do elemento pai (div)
+let texts = [6];
+for (let i = 0; i < 6; i++) {
+    let options = {
+        div: 'main-text-' + i, // id do elemento pai (div)
+        content: data[i], // dados a serem exibidos
+        callback: saveContent, // função de callback para salvar o conteúdo
+    }
+    texts[i] = new Solvertype(options);
+}
+
+// Essa função é chamada quando o usuário altera e salva o conteúdo do editor
+// Deve ser usada pela aplicação para salvar o conteúdo do editor no banco de dados
+function saveContent(id, content) {
+    console.log('id: ' + id); // id do elemento pai (div)
+    console.log('content: ' + content); // conteúdo do editor
+}
---
+++ solvertype.js
@@ -0,0 +1,237 @@
+
+/**
+/* Classe para o editor de texto Solvertype  
+/* Autor: Maurício Garcia, novembro/2023
+/* Utiliza o KaTeX para renderizar o LaTeX
+*/
+
+class Solvertype {
+
+    constructor(options) {
+        this.id = options.div; // id do elemento pai (div)
+        this.content = options.content; // conteúdo do editor
+        this.changed = false; // flag para saber se o conteúdo foi alterado
+        this.callback = options.callback; // função de callback para salvar o conteúdo
+        this.load();
+
+    }
+
+    // carrega o conteúdo do editor no elemento pai (div)
+    load() {
+        let renderedContent = this.render(this.content);
+        let tx = `
+
+        <div class="solvertype-text" id="solvertype-text-${this.id}">
+            <div class="solvertype-text-buttons">
+                <button class="solvertype-text-button" id="solvertype-text-edit-${this.id}" title="Editar">&#9998;</button>
+            </div>
+            <div id="solvertype-text-body-${this.id}">${renderedContent}</div>
+         </div>
+ 
+         <div class="solvertype-input" id="solvertype-input-${this.id}">
+             <div class="solvertype-input-buttons">
+                <div class="solvertype-input-buttons-left">
+                    <button class="solvertype-input-button" id="solvertype-input-bold-${this.id}" title="Negrito"><b>B</b></button>
+                    <button class="solvertype-input-button" id="solvertype-input-italic-${this.id}" title="Itálico"><i>I</i></button>
+                    <button class="solvertype-input-button" id="solvertype-input-underline-${this.id}" title="Itálico"><u>U</u></button>
+                    <button class="solvertype-input-button" id="solvertype-input-superscript-${this.id}" title="Superescrito"><sup>x</sup></button>
+                    <button class="solvertype-input-button" id="solvertype-input-subscript-${this.id}" title="Subescrito"><sub>x</sub></button>
+                    <div class="solvertype-input-button-separator"></div>                   
+                    <button class="solvertype-input-button" id="solvertype-input-clear-${this.id}" title="Remove formatações">&#9938;</button>
+                    <div class="solvertype-input-button-separator"></div>
+                    <div>
+                    <button class="solvertype-input-button" id="solvertype-input-symbol-${this.id}" title="Símbolos">&beta;</button>
+                    <div class="solvertype-input-symbols" id="solvertype-input-symbols-${this.id}"></div>
+                    </div>
+                    <div>
+                    <button class="solvertype-input-button" id="solvertype-input-latex-${this.id}" title="LaTeX">&Sqrt;</button>
+                    <div class="solvertype-input-latexs" id="solvertype-input-latexs-${this.id}"></div>
+                    </div>
+                    <button class="solvertype-input-button" id="solvertype-input-image-${this.id}" title="Imagens">&#9824;</button>
+                </div>
+                <div class="solvertype-input-buttons-right">
+                    <button class="solvertype-input-button" id="solvertype-input-close-${this.id}" title="Fechar">X</button>
+                </div>
+             </div>
+             <div class="solvertype-input-body" id="solvertype-input-body-${this.id}" contenteditable="true">${this.content}</div>
+         </div>
+         `;
+
+        // Carrega o conteúdo no elemento pai (div)
+        document.getElementById(this.id).innerHTML = tx;
+
+        // Esconde o input do editor, para exibir apenas o texto com o botão de editar
+        document.getElementById(`solvertype-input-${this.id}`).style.display = 'none';
+
+        // Muda o status de "changed" quando o conteúdo é alterado
+        document.getElementById(`solvertype-input-body-${this.id}`).addEventListener('keyup', () => this.change());
+
+        // Não permite que o usuário cole um texto formatado
+        document.getElementById(`solvertype-input-body-${this.id}`).addEventListener('paste', (e) => this.paste(e));
+
+        // Fecha o menu se clicar
+        document.getElementById(`solvertype-input-body-${this.id}`).addEventListener('mousedown', (e) => this.closeMenus());
+
+        // Botões do editor
+        document.getElementById(`solvertype-text-edit-${this.id}`).addEventListener('click', () => this.edit());
+        document.getElementById(`solvertype-input-bold-${this.id}`).addEventListener('click', () => this.format('B'));
+        document.getElementById(`solvertype-input-italic-${this.id}`).addEventListener('click', () => this.format('I'));
+        document.getElementById(`solvertype-input-underline-${this.id}`).addEventListener('click', () => this.format('U'));
+        document.getElementById(`solvertype-input-superscript-${this.id}`).addEventListener('click', () => this.format('P'));
+        document.getElementById(`solvertype-input-subscript-${this.id}`).addEventListener('click', () => this.format('S'));
+        document.getElementById(`solvertype-input-clear-${this.id}`).addEventListener('click', () => this.format('X'));
+        document.getElementById(`solvertype-input-symbol-${this.id}`).addEventListener('click', () => this.symbol());
+        document.getElementById(`solvertype-input-latex-${this.id}`).addEventListener('click', () => this.latex());
+        document.getElementById(`solvertype-input-image-${this.id}`).addEventListener('click', () => this.image());
+        document.getElementById(`solvertype-input-close-${this.id}`).addEventListener('click', () => this.close());
+
+    }
+
+    // renderiza o LaTeX usando o KaTeX
+    render(str) {
+        return str.replace(/\[latex\](.*?)\[\/latex\]/g, (match, latex) => {
+            return katex.renderToString(latex, { throwOnError: false });
+        });
+    }
+
+    // edita o texto, abre o editor
+    edit() {
+        // fecha todos os editores e abre todos os textos
+        this.closeMenus();
+        const tbox = document.querySelectorAll('.solvertype-text');
+        const ibox = document.querySelectorAll('.solvertype-input');
+        for (let i = 0; i < ibox.length; i++) {
+            tbox[i].style.display = 'block';
+            ibox[i].style.display = 'none';
+        }
+        // fecha o texto selecionado
+        document.getElementById(`solvertype-text-${this.id}`).style.display = 'none';
+        // abre o editor selecionado
+        document.getElementById(`solvertype-input-${this.id}`).style.display = 'block';
+    }
+
+    // remove formatações ao colar um texto
+    paste(e) {
+        this.closeMenus();
+        e.preventDefault();
+        let text = e.clipboardData.getData('text/plain');
+        document.execCommand('insertText', false, text);
+        this.changed = true;
+    }
+
+    //formatações do texto
+    format(op) {
+        this.closeMenus();
+        if (op == 'B') {
+            document.execCommand('bold');
+        } else if (op == 'I') {
+            document.execCommand('italic');
+        } else if (op == 'U') {
+            document.execCommand('underline');
+        } else if (op == 'P') {
+            document.execCommand('superscript');
+        } else if (op == 'S') {
+            document.execCommand('subscript');
+        } else if (op == 'X') {
+            document.execCommand('removeFormat');
+        }
+        this.changed = true;
+    }
+
+    // símbolos
+    symbol() {
+        if (document.getElementById(`solvertype-input-symbols-${this.id}`).style.display == 'block') {
+            this.closeMenus();
+            return;
+        }
+        const symbols = ['&alpha;', '&beta;', '&gamma;', '&Gamma;', '&delta;', '&Delta;', '&epsilon;', '&theta;', '&Theta;', '&lambda;', '&Lambda;', '&mu;', '&nu;', '&xi;', '&pi;', '&sigma;', '&Sigma;', '&tau;', '&phi;', '&Phi;', '&psi;', '&Psi;', '&omega;', '&Omega;', '&larr;', '&uarr;', '&rarr;', '&darr;', '&rArr;', '&hArr;', '&#8596;', '&#8629;', '&forall;', '&part;', '&exist;', '&nabla;', '&isin;', '&ni;', '&sum;', '&radic;', '&prop;', '&infin;', '&ang;', '&and;', '&or;', '&cap;', '&cup;', '&int;', '&there4;', '&ne;', '&#8804;', '&#8805;', '&equiv;', '&lt;', '&gt;', '&#8773;', '&#8776;', '&plusmn;', '&times;', '&divide;', '&amp;', '&sub;', '&sup;', '&sube;', '&supe;', '&perp;', '&lsquo;', '&rsquo;', '&ldquo;', '&rdquo;', '&#171;', '&#187;', '&dagger;', '&Dagger;', '&permil;', '&lsaquo;', '&rsaquo;', '&not;', '&pound;', '&sect;', '&copy;', '&reg;', '&#8482;', '&micro;', '&para;', '&Oslash;', '&oslash;', '&#185;', '&sup2;', '&sup3;', '&frac14;', '&frac12;', '&frac34;', '&deg;', '&#186;', '&#170;', '&#191;', '&#161;', '&#8709;', '&#8713;', '&#8836;', '&#8853;', '&#8855;', '&#338;', '&#339;', '&#9674;', '&#9824;', '&#9827;', '&#9829;', '&#9830;'];
+        let tx_symbols = '';
+        let r = 0
+        for (let i = 0; i < symbols.length; i++) {
+            tx_symbols += `<button class="solvertype-input-symbol" value="${symbols[i]}">${symbols[i]}</button>`;
+            if (r < 9) {
+                r = r + 1;
+            }
+            else {
+                tx_symbols += '<br>';
+                r = 0;
+            }
+        }
+        document.getElementById(`solvertype-input-symbols-${this.id}`).innerHTML = tx_symbols;
+        document.getElementById(`solvertype-input-symbols-${this.id}`).style.display = 'block';
+        document.getElementById(`solvertype-input-latexs-${this.id}`).style.display = 'none';
+        const cbox = document.querySelectorAll('.solvertype-input-symbol');
+        for (let i = 0; i < cbox.length; i++) {
+            cbox[i].addEventListener("click", e => {
+                document.execCommand('insertText', false, e.target.value);
+                this.changed = true;
+                document.getElementById(`solvertype-input-symbols-${this.id}`).style.display = 'none';
+            });
+        }
+    }
+
+    // LaTeX
+    latex() {
+        if (document.getElementById(`solvertype-input-latexs-${this.id}`).style.display == 'block') {
+            this.closeMenus();
+            return;
+        }
+        const latexs = ['L', '&#189;', '&plusmn;', '&radic;', '&#8731;', '&#775;', '&#803;'];
+        const latexs_values = ['[latex][/latex]', '\\frac{1}{2}', '\\pm', '\\sqrt{x}', '\\sqrt[3]{x}', '^', '_'];
+        let tx_latexs = '';
+        let r = 0
+        for (let i = 0; i < latexs.length; i++) {
+            tx_latexs += `<button class="solvertype-input-latex" value="${latexs_values[i]}">${latexs[i]}</button>`;
+            if (r < 9) {
+                r = r + 1;
+            }
+            else {
+                tx_latexs += '<br>';
+                r = 0;
+            }
+        }
+        document.getElementById(`solvertype-input-latexs-${this.id}`).innerHTML = tx_latexs;
+        document.getElementById(`solvertype-input-latexs-${this.id}`).style.display = 'block';
+        document.getElementById(`solvertype-input-symbols-${this.id}`).style.display = 'none';
+        const cbox = document.querySelectorAll('.solvertype-input-latex');
+        for (let i = 0; i < cbox.length; i++) {
+            cbox[i].addEventListener("click", e => {
+                document.execCommand('insertText', false, e.target.value);
+                this.changed = true;
+                document.getElementById(`solvertype-input-latexs-${this.id}`).style.display = 'none';
+            });
+        }
+    }
+
+    // imagens
+    image() {
+        alert('image');
+    }
+
+    // fecha o editor
+    close() {
+        if (this.changed) {
+            if (confirm('Deseja salvar as alterações?')) {
+                this.content = document.getElementById(`solvertype-input-body-${this.id}`).innerHTML;
+                this.callback(this.id, this.content);
+            }
+            this.changed = false;
+        }
+        this.load();
+    }
+
+    // fecha os menus abertos
+    closeMenus() {
+        document.getElementById(`solvertype-input-symbols-${this.id}`).style.display = 'none';
+        document.getElementById(`solvertype-input-latexs-${this.id}`).style.display = 'none';
+    }
+
+    change() {
+        this.closeMenus();
+        this.changed = true;
+    }
+}
+
+
+
+export default Solvertype;
